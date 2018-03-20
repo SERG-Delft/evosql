8554ea6c9bde412046c3b0fedf0b4504cc5f9a2d
Paul <p.w.p.vanderstel@student.tudelft.nl>
2018-03-20 10:28:43 +0100

    Add JavaDoc to VendorOptions

--- brew/src/main/java/nl/tudelft/serg/evosql/brew/sql/VendorOptions.java
+++ brew/src/main/java/nl/tudelft/serg/evosql/brew/sql/VendorOptions.java
@@ -1,7 +1,15 @@
 package nl.tudelft.serg.evosql.brew.sql;
 
+/**
+ * An interface for obtaining vendor-specific SQL syntax.
+ */
 public interface VendorOptions {
 
-    String escapeTableName(String query);
-
+    /**
+     * Escapes a table name for the correct vendor.
+     *
+     * @param tableName The name of the table.
+     * @return An escaped version of the table name.
+     */
+    String escapeTableName(String tableName);
 }

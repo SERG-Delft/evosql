fa08b006459e5a9c05fb5cf7310256c756e653e0
Paul <p.w.p.vanderstel@student.tudelft.nl>
2018-03-20 10:25:21 +0100

    Add JavaDoc to QueryBuilder

--- brew/src/main/java/nl/tudelft/serg/evosql/brew/sql/QueryBuilder.java
+++ brew/src/main/java/nl/tudelft/serg/evosql/brew/sql/QueryBuilder.java
@@ -7,14 +7,31 @@ import nl.tudelft.serg.evosql.brew.data.Result;
 
 import java.util.List;
 
+/**
+ * An abstract class for transforming data into SQL queries.
+ */
 @Data
 public abstract class QueryBuilder {
 
+    /**
+     * The vendor options used for SQL generation.
+     */
     @NonNull
     private final VendorOptions vendorOptions;
 
+    /**
+     * Builds a list of SQL queries using result data.
+     *
+     * @param result The result data to use for SQL generation.
+     * @return A list of SQL queries.
+     */
     public abstract List<String> buildQueries(Result result);
 
+    /**
+     * Builds a single aggregate SQL query using result data.
+     *
+     * @param result The result data to use for SQL generation.
+     * @return A single SQL query.
+     */
     public abstract String buildAggregateQuery(Result result);
-
 }

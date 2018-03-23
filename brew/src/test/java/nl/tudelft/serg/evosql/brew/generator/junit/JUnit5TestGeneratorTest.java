1ba15bc12f9212edf7cee1b97372e74bb8d0ddd2
Paul <p.w.p.vanderstel@student.tudelft.nl>
2018-03-23 16:12:42 +0100

    Extract common functionality to one class

---
+++ brew/src/test/java/nl/tudelft/serg/evosql/brew/generator/junit/JUnit5TestGeneratorTest.java
@@ -0,0 +1,54 @@
+package nl.tudelft.serg.evosql.brew.generator.junit;
+
+import nl.tudelft.serg.evosql.brew.DataGenerator;
+import nl.tudelft.serg.evosql.brew.data.Result;
+import nl.tudelft.serg.evosql.brew.db.ConnectionData;
+import nl.tudelft.serg.evosql.brew.sql.vendor.MySQLOptions;
+import org.junit.jupiter.api.Test;
+
+public class JUnit5TestGeneratorTest {
+    private Result resultSmall = DataGenerator.makeResult1();
+    private Result resultMedium = DataGenerator.makeResult2();
+
+    @Test
+    public void testMethodgenerationSmall() {
+        ConnectionData connectionData = new ConnectionData(
+                "connection string",
+                "db",
+                "user",
+                "password"
+        );
+        JUnitGeneratorSettings JUnitGeneratorSettings = new JUnitGeneratorSettings(
+                connectionData,
+                "brew.test.package",
+                true,
+                true,
+                true,
+                true,
+                true
+        );
+        JUnit5TestGenerator jUnit4TestGenerator = new JUnit5TestGenerator(JUnitGeneratorSettings);
+        System.out.println(jUnit4TestGenerator.generate(resultSmall, new MySQLOptions()));
+    }
+
+    @Test
+    public void testMethodgenerationMedium() {
+        ConnectionData connectionData = new ConnectionData(
+                "connection string",
+                "db",
+                "user",
+                "password"
+        );
+        JUnitGeneratorSettings JUnitGeneratorSettings = new JUnitGeneratorSettings(
+                connectionData,
+                "brew.test.package",
+                true,
+                true,
+                true,
+                true,
+                true
+        );
+        JUnit5TestGenerator jUnit4TestGenerator = new JUnit5TestGenerator(JUnitGeneratorSettings);
+        System.out.println(jUnit4TestGenerator.generate(resultMedium, new MySQLOptions()));
+    }
+}

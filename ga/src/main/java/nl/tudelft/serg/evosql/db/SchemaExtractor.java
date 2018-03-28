c8678f88bc0457245a2448e5c4ddf85bb7b32bd4
Paul <p.w.p.vanderstel@student.tudelft.nl>
2018-03-28 14:49:33 +0200

    Make the big if-statement a switch
    This allows for more optimization

--- ga/src/main/java/nl/tudelft/serg/evosql/db/SchemaExtractor.java
+++ ga/src/main/java/nl/tudelft/serg/evosql/db/SchemaExtractor.java
@@ -26,127 +26,143 @@ import nl.tudelft.serg.evosql.sql.parser.TablesNamesFinder;
 
 public class SchemaExtractor implements ISchemaExtractor {
 
-	private static Logger log = LogManager.getLogger(SchemaExtractor.class);
-	
-	private Map<String, TableSchema> knownTables;
-	
-	private String jdbc;
-	private String user;
-	private String pwd;
+    private static Logger log = LogManager.getLogger(SchemaExtractor.class);
 
-	private String database;
+    private Map<String, TableSchema> knownTables;
 
-	public SchemaExtractor(String jdbc, String database, String user, String pwd) {
-		this.jdbc = jdbc;
-		this.database = database;
-		this.user = user;
-		this.pwd = pwd;
-		
-		knownTables = new HashMap<String, TableSchema>();
-	}
-	
-	@Override
-	public TableSchema extract(String table) {
-		if (knownTables.containsKey(table)) {
-			return knownTables.get(table);
-		}
-		
-		Connection con=null;
-		log.debug("Extracting schema from table: " + table);
-		try {
-			con = DriverManager.getConnection(jdbc, user, pwd);
-			
-			DatabaseMetaData metaData = con.getMetaData();
+    private String jdbc;
+    private String user;
+    private String pwd;
 
-			ResultSet result = metaData.getTables(database, null, "%", new String[] { "TABLE" });
-			while (result.next()) {
-				String tableName = result.getString(3);
-				if(!tableName.toUpperCase().equals(table.toUpperCase())) continue;
-				
-				ResultSet columns = metaData.getColumns(database, null, tableName, null);
+    private String database;
 
-				List<ColumnSchema> parsedColumns = new ArrayList<>();		
-				TableSchema ts = new TableSchema(tableName, parsedColumns);		
-				
-				while (columns.next()) {
-					String columnName = columns.getString(4);
-					int dataType = columns.getInt(5);
-					int length = columns.getInt(7);
-					int nullable = columns.getInt(11);
-					String autoIncrement = columns.getString(23);
+    public SchemaExtractor(String jdbc, String database, String user, String pwd) {
+        this.jdbc = jdbc;
+        this.database = database;
+        this.user = user;
+        this.pwd = pwd;
 
-					DBType dbDataType = createDBType(dataType, length);
-					
-					log.debug("Field {}={}", columnName, dbDataType.getClass().getSimpleName());
+        knownTables = new HashMap<String, TableSchema>();
+    }
 
-					parsedColumns.add(new ColumnSchema(ts, columnName, dbDataType, nullable==1?true:false, autoIncrement.toLowerCase().equals("yes")?true:false));
-				}
-				
-				log.debug("Columns found: " + parsedColumns.size());
-				log.debug("Table schema: " + parsedColumns);
-				knownTables.put(table, ts);
-				return ts;
-			}
-			
-			throw new RuntimeException("table " + table + " doesn't exist");
+    @Override
+    public TableSchema extract(String table) {
+        if (knownTables.containsKey(table)) {
+            return knownTables.get(table);
+        }
 
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			try {
-				if(con!=null) con.close();
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
-	}
+        Connection con = null;
+        log.debug("Extracting schema from table: " + table);
+        try {
+            con = DriverManager.getConnection(jdbc, user, pwd);
 
-	private DBType createDBType(int dataType, int length) {
-		DBType dbDataType = null;
-		if(dataType == Types.DOUBLE) dbDataType= new DBDouble();
-		else if(dataType == Types.REAL) dbDataType = new DBDouble("REAL");
-		else if(dataType == Types.DECIMAL) dbDataType = new DBDouble("DECIMAL");
-		else if(dataType == Types.INTEGER) dbDataType = new DBInteger();
-		else if(dataType == Types.SMALLINT) dbDataType = new DBInteger("SMALLINT");
-		else if(dataType == Types.VARCHAR) dbDataType = new DBString(length);
-		else if(dataType == Types.LONGVARCHAR) dbDataType = new DBString(EvoSQLConfiguration.MAX_STRING_LENGTH, "LONGVARCHAR");
-		else if(dataType == Types.CHAR)	dbDataType = new DBString(length, "CHAR");
-		else if(dataType == Types.BOOLEAN) dbDataType = new DBBoolean("BOOLEAN");
-		else if(dataType == Types.DATE) dbDataType = new DBDate();
-		else if(dataType == Types.TIME) dbDataType = new DBTime();
-		else if(dataType == Types.TIMESTAMP) dbDataType = new DBDateTime("TIMESTAMP"); //datetime
-		else if(dataType == Types.TINYINT) dbDataType = new DBInteger("TINYINT"); // HSQLDB doesnt support tinyint
-		else if(dataType == Types.BIT) dbDataType = new DBBoolean(); // HSQLDB doesnt support bit
-		else if(dataType == Types.BIGINT) dbDataType = new DBInteger(); // we dont need bigint in our evaluations
-		else if(dataType == Types.ARRAY) throw new RuntimeException("EvoSQL lacks a ARRAY implementation");
-		else throw new RuntimeException("DB type not supported: " + dataType);
-		
-		return dbDataType;
-	}
+            DatabaseMetaData metaData = con.getMetaData();
 
-	@Override
-	public Map<String, TableSchema> getTablesFromQuery(String pathToBeTested) {
-		Map<String, TableSchema> tableSchemas = new HashMap<String, TableSchema>();
-		
-		// Get a list of table names from the query
-		Statement stmt;
-		try {
-			stmt = CCJSqlParserUtil.parse(pathToBeTested);
-		} catch (JSQLParserException e) {
-			e.printStackTrace();
-			return null;
-		}
-		
-		if (!(stmt instanceof Select)) {
-			return null;
-		}
-		List<String> tableList = new TablesNamesFinder().getTableList(stmt);
-		
-		for (String tableName : tableList) {
-			tableName = tableName.replaceAll("^\"|\"$", ""); // Remove quotes around tablenames
-			tableSchemas.put(tableName,	this.extract(tableName));
-		}
-		
-		return tableSchemas;
-	}
+            ResultSet result = metaData.getTables(database, null, "%", new String[]{"TABLE"});
+            while (result.next()) {
+                String tableName = result.getString(3);
+                if (!tableName.toUpperCase().equals(table.toUpperCase())) continue;
+
+                ResultSet columns = metaData.getColumns(database, null, tableName, null);
+
+                List<ColumnSchema> parsedColumns = new ArrayList<>();
+                TableSchema ts = new TableSchema(tableName, parsedColumns);
+
+                while (columns.next()) {
+                    String columnName = columns.getString(4);
+                    int dataType = columns.getInt(5);
+                    int length = columns.getInt(7);
+                    int nullable = columns.getInt(11);
+                    String autoIncrement = columns.getString(23);
+
+                    DBType dbDataType = createDBType(dataType, length);
+
+                    log.debug("Field {}={}", columnName, dbDataType.getClass().getSimpleName());
+
+                    parsedColumns.add(new ColumnSchema(ts, columnName, dbDataType, nullable == 1 ? true : false, autoIncrement.toLowerCase().equals("yes") ? true : false));
+                }
+
+                log.debug("Columns found: " + parsedColumns.size());
+                log.debug("Table schema: " + parsedColumns);
+                knownTables.put(table, ts);
+                return ts;
+            }
+
+            throw new RuntimeException("table " + table + " doesn't exist");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (con != null) con.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private DBType createDBType(int dataType, int length) {
+        switch (dataType) {
+            case Types.DOUBLE:
+                return new DBDouble();
+            case Types.REAL:
+                return new DBDouble("REAL");
+            case Types.DECIMAL:
+                return new DBDouble("DECIMAL");
+            case Types.INTEGER:
+                return new DBInteger();
+            case Types.SMALLINT:
+                return new DBInteger("SMALLINT");
+            case Types.TINYINT:
+                return new DBInteger("TINYINT"); // HSQLDB doesn't support tinyint
+            case Types.BIGINT:
+                return new DBInteger(); // we don't need bigint in our evaluations
+            case Types.VARCHAR:
+                return new DBString(length);
+            case Types.LONGVARCHAR:
+                return new DBString(EvoSQLConfiguration.MAX_STRING_LENGTH, "LONGVARCHAR");
+            case Types.CHAR:
+                return new DBString(length, "CHAR");
+            case Types.BIT:
+                return new DBBoolean(); // HSQLDB doesn't support bit
+            case Types.BOOLEAN:
+                return new DBBoolean("BOOLEAN");
+            case Types.DATE:
+                return new DBDate();
+            case Types.TIME:
+                return new DBTime();
+            case Types.TIMESTAMP:
+                return new DBDateTime("TIMESTAMP");
+            case Types.ARRAY:
+                throw new RuntimeException("EvoSQL currently lacks an ARRAY implementation.");
+            default:
+                throw new RuntimeException("This database type is not currently supported: " + dataType);
+        }
+    }
+
+    @Override
+    public Map<String, TableSchema> getTablesFromQuery(String pathToBeTested) {
+        Map<String, TableSchema> tableSchemas = new HashMap<String, TableSchema>();
+
+        // Get a list of table names from the query
+        Statement stmt;
+        try {
+            stmt = CCJSqlParserUtil.parse(pathToBeTested);
+        } catch (JSQLParserException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        if (!(stmt instanceof Select)) {
+            return null;
+        }
+        List<String> tableList = new TablesNamesFinder().getTableList(stmt);
+
+        for (String tableName : tableList) {
+            tableName = tableName.replaceAll("^\"|\"$", ""); // Remove quotes around tablenames
+            tableSchemas.put(tableName, this.extract(tableName));
+        }
+
+        return tableSchemas;
+    }
 }

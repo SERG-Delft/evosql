b38d33db140b69fd6b388f58994e105640d2c530
Paul <p.w.p.vanderstel@student.tudelft.nl>
2018-04-09 19:17:54 +0200

    Add JavaDoc to FileConsumer

--- brew/src/main/java/nl/tudelft/serg/evosql/brew/consumer/FileConsumer.java
+++ brew/src/main/java/nl/tudelft/serg/evosql/brew/consumer/FileConsumer.java
@@ -11,20 +11,42 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+/**
+ * An OutputConsumer that writes the output to files.
+ */
 public class FileConsumer implements OutputConsumer {
+    /**
+     * The directory to write to.
+     */
     @Getter
     private final Path directory;
     private final FileOutputStreamProvider fileOutputStreamProvider;
 
+    /**
+     * Initializes a new instance of the FileConsumer class.
+     *
+     * @param directory The directory to write to.
+     */
     public FileConsumer(Path directory) {
         this(directory, new FileOutputStreamProvider());
     }
 
+    /**
+     * Initializes a new instance of the FileConsumer class.
+     *
+     * @param directory                The directory to write to.
+     * @param fileOutputStreamProvider The provider for file output streams.
+     */
     public FileConsumer(@NonNull Path directory, FileOutputStreamProvider fileOutputStreamProvider) {
         this.directory = directory;
         this.fileOutputStreamProvider = fileOutputStreamProvider;
     }
 
+    /**
+     * Writes the output of a generator to the filesystem.
+     *
+     * @param outputs The list of output objects to write.
+     */
     @Override
     public void consumeOutput(List<Output> outputs) {
         try {
@@ -44,7 +66,17 @@ public class FileConsumer implements OutputConsumer {
         }
     }
 
+    /**
+     * A helper class for initializing file output streams.
+     */
     public static class FileOutputStreamProvider {
+        /**
+         * Initializes a new instance of a file output stream.
+         *
+         * @param file The file to create a stream for.
+         * @return An output stream for the given file.
+         * @throws FileNotFoundException if (part of) the path to the given file does not exist.
+         */
         public FileOutputStream createStream(File file) throws FileNotFoundException {
             return new FileOutputStream(file);
         }

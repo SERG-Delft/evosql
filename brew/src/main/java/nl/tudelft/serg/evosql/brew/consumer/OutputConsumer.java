95d5fc70750bd4865b13b8e5f744f558466c4161
Paul <p.w.p.vanderstel@student.tudelft.nl>
2018-04-09 15:48:48 +0200

    Add JavaDoc to OutputConsumer

--- brew/src/main/java/nl/tudelft/serg/evosql/brew/consumer/OutputConsumer.java
+++ brew/src/main/java/nl/tudelft/serg/evosql/brew/consumer/OutputConsumer.java
@@ -4,6 +4,14 @@ import nl.tudelft.serg.evosql.brew.generator.Output;
 
 import java.util.List;
 
+/**
+ * An interface for consuming generator outputs.
+ */
 public interface OutputConsumer {
+    /**
+     * Consumes the output of a generator.
+     *
+     * @param outputs The list of output objects to consume.
+     */
     void consumeOutput(List<Output> outputs);
 }

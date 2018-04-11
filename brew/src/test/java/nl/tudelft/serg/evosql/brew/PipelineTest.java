210245aaac1c7801ffefe2d00e7fc464eedc1848
Paul <p.w.p.vanderstel@student.tudelft.nl>
2018-04-11 10:38:39 +0200

    Update tests for Pipeline

--- brew/src/test/java/nl/tudelft/serg/evosql/brew/PipelineTest.java
+++ brew/src/test/java/nl/tudelft/serg/evosql/brew/PipelineTest.java
@@ -8,6 +8,7 @@ import nl.tudelft.serg.evosql.brew.generator.Generator;
 import nl.tudelft.serg.evosql.brew.generator.Output;
 import nl.tudelft.serg.evosql.brew.sql.vendor.PostgreSQLOptions;
 import nl.tudelft.serg.evosql.brew.sql.vendor.VendorOptions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -17,6 +18,7 @@ import static org.mockito.Mockito.*;
 
 public class PipelineTest {
     @Test
+    @DisplayName("Methods should not be called more than once")
     void pipelineTest() {
         final QueryRunner queryRunner = mock(QueryRunner.class);
         final String sql = "Select * From any;";
@@ -35,9 +37,7 @@ public class PipelineTest {
                 .queryRunner(queryRunner)
                 .connectionData(connectionData)
                 .sqlQuery(sql)
-                .generator(generator)
-                .vendorOptions(vendorOptions)
-                .outputConsumer(outputConsumer)
+                .resultProcessor(new Pipeline.ResultProcessor(generator, vendorOptions, outputConsumer))
                 .build();
 
         p.execute();
@@ -45,5 +45,38 @@ public class PipelineTest {
         verify(queryRunner, times(1)).runQuery(sql, connectionData);
         verify(generator, times(1)).generate(result, vendorOptions);
         verify(outputConsumer, times(1)).consumeOutput(outputs);
+    }
+
+    @Test
+    @DisplayName("The same generator should be called once")
+    void generatorCacheTest() {
+        final QueryRunner queryRunner = mock(QueryRunner.class);
+        final String sql = "Select * From any;";
+        final ConnectionData connectionData = new ConnectionData("", "", "", "");
+        final Result result = mock(Result.class);
+        when(queryRunner.runQuery(sql, connectionData)).thenReturn(result);
+
+        final Generator generator = mock(Generator.class);
+        final VendorOptions vendorOptions = new PostgreSQLOptions();
+        final List<Output> outputs = Arrays.asList(new Output("1", "one"), new Output("2", "two"));
+        when(generator.generate(result, vendorOptions)).thenReturn(outputs);
+
+        final OutputConsumer outputConsumer1 = mock(OutputConsumer.class);
+        final OutputConsumer outputConsumer2 = mock(OutputConsumer.class);
+
+        Pipeline p = Pipeline.builder()
+                .queryRunner(queryRunner)
+                .connectionData(connectionData)
+                .sqlQuery(sql)
+                .resultProcessor(new Pipeline.ResultProcessor(generator, vendorOptions, outputConsumer1))
+                .resultProcessor(new Pipeline.ResultProcessor(generator, vendorOptions, outputConsumer2))
+                .build();
+
+        p.execute();
+
+        verify(queryRunner, times(1)).runQuery(sql, connectionData);
+        verify(generator, times(1)).generate(result, vendorOptions);
+        verify(outputConsumer1, times(1)).consumeOutput(outputs);
+        verify(outputConsumer2, times(1)).consumeOutput(outputs);
     }
 }

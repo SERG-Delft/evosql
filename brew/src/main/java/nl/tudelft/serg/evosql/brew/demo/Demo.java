450172eadba4bbd1926507b3dd550b60b5ecceef
Daniël van Gelder <d.vangelder-1@student.tudelft.nl>
2018-04-11 11:18:34 +0200

    Add comments to clarify behaviour

--- brew/src/main/java/nl/tudelft/serg/evosql/brew/demo/Demo.java
+++ brew/src/main/java/nl/tudelft/serg/evosql/brew/demo/Demo.java
@@ -14,17 +14,21 @@ import java.nio.file.Paths;
 
 public class Demo {
 
+    /**
+     * This is the query on which we will generate tests.
+     */
     final static String QUERY_TO_RUN = "SELECT * FROM Products t WHERE t.Price < (SELECT MAX(Type) FROM product_detail t2 WHERE t.Product = t2.Name) AND t.Price > (SELECT MAX(Type) FROM product_detail t2 WHERE t.Product = t2.Name) - 10";
 
     public static void main(String[] args) {
+        //Connection data to production database
         ConnectionData connectionDataProd = new ConnectionData(
                 "jdbc:postgresql://localhost:5432/evosql_brew_prod",
                 "evosql_brew_prod", "postgres", "");
-//?nullNamePatternMatchesAll=true
-
+        //Connection data to test database (this is where the test cases make a connection to
         ConnectionData connectionDataTest = new ConnectionData(
                 "jdbc:postgresql://localhost:5432/evosql_brew_test",
                 "evosql_brew_test", "postgres", "");
+        //Configure jUnitGenerator
         JUnitGeneratorSettings jUnitGeneratorSettings = new JUnitGeneratorSettings(
                 connectionDataTest,
                 "brew.test",
@@ -33,9 +37,8 @@ public class Demo {
                 true,
                 true,
                 false,
-                true
-        );
-
+                true);
+        //Construct Pipeline
         Pipeline pipeline = Pipeline.builder()
                 .queryRunner(new EvoSQLRunner())
                 .connectionData(connectionDataProd)
@@ -45,7 +48,7 @@ public class Demo {
                         new PostgreSQLOptions(),
                         new FileConsumer(Paths.get("."))))
                 .build();
-
+        //Execute pipeline
         pipeline.execute();
     }
 }

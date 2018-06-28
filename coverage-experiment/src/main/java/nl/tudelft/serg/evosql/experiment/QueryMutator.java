31cb2b0771cef4ba28ebd8a4b689c5dd617634b0
Daniël van Gelder <d.vangelder-1@student.tudelft.nl>
2018-06-28 14:40:05 +0200

    Skeleton Code for query mutator class

--- coverage-experiment/src/main/java/nl/tudelft/serg/evosql/experiment/QueryMutator.java
+++ coverage-experiment/src/main/java/nl/tudelft/serg/evosql/experiment/QueryMutator.java
@@ -1,7 +1,14 @@
 package nl.tudelft.serg.evosql.experiment;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Class responsible for mutating the conditions in the query.
  */
 public class QueryMutator {
+
+    public List<String> parseMutations(String xml) {
+        return Arrays.asList();
+    }
 }

e62eb9817b808d15bca11bb0945296ebe692256b
Daniël van Gelder <d.vangelder-1@student.tudelft.nl>
2018-06-28 14:40:41 +0200

    Skeleton code for web service connection

--- coverage-experiment/src/main/java/nl/tudelft/serg/evosql/experiment/WebMutatorConnector.java
+++ coverage-experiment/src/main/java/nl/tudelft/serg/evosql/experiment/WebMutatorConnector.java
@@ -1,4 +1,48 @@
 package nl.tudelft.serg.evosql.experiment;
 
+
+import in2test.application.common.SQLToolsConfig;
+import in2test.application.services.SQLMutationWSFacade;
+import nl.tudelft.serg.evosql.sql.parser.SqlSecurer;
+
+/**
+ * Class responsible for making a web request to retrieve the query mutations
+ * from the SQLMutation web service.
+ */
 public class WebMutatorConnector {
+    private String secureSqlquery;
+    private String schemaXml;
+
+    public WebMutatorConnector(String sqlQuery, String schema) {
+        SqlSecurer sqlSecurer = new SqlSecurer(sqlQuery);
+        secureSqlquery = sqlSecurer.getSecureSql();
+        schemaXml = getSchemaXml(schema);
+        SQLToolsConfig.configure();
+    }
+
+    /**
+     * Transforms the schema into XML format.
+     *
+     * @param schema schema of query
+     * @return schema in XML format.
+     */
+    private String getSchemaXml(String schema) {
+        return schema;
+    }
+
+
+    /**
+     * Calls the SQLMutation web service in order to get the mutants of the query.
+     * The mutants are in XML format.
+     *
+     * @return XML representation of the web service.
+     */
+    public String requestMutants() {
+        String mutantsXml = "";
+        SQLMutationWSFacade mutationWSFacade = new SQLMutationWSFacade();
+        mutantsXml = mutationWSFacade.getMutants(secureSqlquery, schemaXml, "");
+        return mutantsXml;
+    }
+
+
 }

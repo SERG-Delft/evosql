5cd915a3a35dd299806877e67af47902fd7d3ed5
danielvangelder <dvangelder4@gmail.com>
2018-07-16 23:43:55 +0200

    Modify QueryExperimentResult for multiple mutants

--- coverage-experiment/src/main/java/nl/tudelft/serg/evosql/experiment/QueryExperimentResult.java
+++ coverage-experiment/src/main/java/nl/tudelft/serg/evosql/experiment/QueryExperimentResult.java
@@ -2,18 +2,21 @@ package nl.tudelft.serg.evosql.experiment;
 
 import lombok.Data;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Object describing the result of executing the experiment for a single query.
- * Contains information about the original query and the mutated query. Also
+ * Contains information about the original query and the mutated queries. Also
  * contains information about the file that it originates from.
  * <p>
- * It also defines a boolean if it passed on the test suite for the original query and another
- * boolean if it passed on the same test suite with the mutated query.
+ * It also defines a boolean for the original query if it passed on the test suite for the original query and a
+ * map with a boolean for each mutated query if it passed on the same test suite with the mutated query.
  */
 @Data
 public class QueryExperimentResult {
     private final String originalQuery;
-    private final String mutatedQuery;
+    private final List<String> mutatedQueries;
     private final boolean initBrewPass;
-    private final boolean mutatedBrewPass;
+    private final Map<String, Boolean> mutatedBrewPass;
 }

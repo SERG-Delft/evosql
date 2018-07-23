019db048574692428cb092765708f495804d5203
Paul <p.w.p.vanderstel@student.tudelft.nl>
2018-07-23 20:50:15 +0200

    Force recreating the containers

--- coverage-experiment/run-experiment.ps1
+++ coverage-experiment/run-experiment.ps1
@@ -1,4 +1,4 @@
 $env:COVERAGE_EXPERIMENT_START = 50;
 $env:COVERAGE_EXPERIMENT_STEP = 100;
 
-docker-compose up --build --abort-on-container-exit;
+docker-compose up --build --abort-on-container-exit --force-recreate;

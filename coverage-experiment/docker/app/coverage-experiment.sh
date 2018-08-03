c778e24cc361d86e913af7c1a3f3e5cf38f23a63
Paul <p.w.p.vanderstel@student.tudelft.nl>
2018-08-03 13:07:17 +0200

    Update scripts to reflect new API

--- coverage-experiment/docker/app/coverage-experiment.sh
+++ coverage-experiment/docker/app/coverage-experiment.sh
@@ -6,7 +6,10 @@ echo COVERAGE_EXPERIMENT_STEP is $COVERAGE_EXPERIMENT_STEP
 echo COVERAGE_EXPERIMENT_STOP is $COVERAGE_EXPERIMENT_STOP
 
 # Run the experiment
-java -jar /root/coverage-experiment-all.jar $COVERAGE_EXPERIMENT_START $COVERAGE_EXPERIMENT_STEP $COVERAGE_EXPERIMENT_STOP
+for i in {$COVERAGE_EXPERIMENT_START..$COVERAGE_EXPERIMENT_STOP..$COVERAGE_EXPERIMENT_STEP}
+do
+    java -jar /root/coverage-experiment-all.jar $i
+done
 
 # Package all experiment generated files into
 # one archive to allow easy copying and portability

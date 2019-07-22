71c1f68d28bb8278922dba237d31ed9b4670e774
Dylan Franken <dtfranken@gmail.com>
2019-07-22 15:33:27 +0200

    Update QueryStripperVisitor

--- evaluation/src/main/java/nl/tudelft/serg/evosql/evaluation/tools/QueryStripperVisitor.java
+++ evaluation/src/main/java/nl/tudelft/serg/evosql/evaluation/tools/QueryStripperVisitor.java
@@ -1,94 +1,21 @@
 package nl.tudelft.serg.evosql.evaluation.tools;
 
+import net.sf.jsqlparser.expression.*;
+import net.sf.jsqlparser.expression.operators.arithmetic.*;
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
+import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
+import net.sf.jsqlparser.expression.operators.relational.*;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.select.*;
+import net.sf.jsqlparser.statement.values.ValuesStatement;
+
 import java.sql.Date;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
-
-import net.sf.jsqlparser.expression.AllComparisonExpression;
-import net.sf.jsqlparser.expression.AnalyticExpression;
-import net.sf.jsqlparser.expression.AnyComparisonExpression;
-import net.sf.jsqlparser.expression.CaseExpression;
-import net.sf.jsqlparser.expression.CastExpression;
-import net.sf.jsqlparser.expression.DateTimeLiteralExpression;
-import net.sf.jsqlparser.expression.DateValue;
-import net.sf.jsqlparser.expression.DoubleValue;
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.ExpressionVisitor;
-import net.sf.jsqlparser.expression.ExtractExpression;
-import net.sf.jsqlparser.expression.Function;
-import net.sf.jsqlparser.expression.HexValue;
-import net.sf.jsqlparser.expression.IntervalExpression;
-import net.sf.jsqlparser.expression.JdbcNamedParameter;
-import net.sf.jsqlparser.expression.JdbcParameter;
-import net.sf.jsqlparser.expression.JsonExpression;
-import net.sf.jsqlparser.expression.KeepExpression;
-import net.sf.jsqlparser.expression.LongValue;
-import net.sf.jsqlparser.expression.MySQLGroupConcat;
-import net.sf.jsqlparser.expression.NullValue;
-import net.sf.jsqlparser.expression.NumericBind;
-import net.sf.jsqlparser.expression.OracleHierarchicalExpression;
-import net.sf.jsqlparser.expression.OracleHint;
-import net.sf.jsqlparser.expression.Parenthesis;
-import net.sf.jsqlparser.expression.RowConstructor;
-import net.sf.jsqlparser.expression.SignedExpression;
-import net.sf.jsqlparser.expression.StringValue;
-import net.sf.jsqlparser.expression.TimeKeyExpression;
-import net.sf.jsqlparser.expression.TimeValue;
-import net.sf.jsqlparser.expression.TimestampValue;
-import net.sf.jsqlparser.expression.UserVariable;
-import net.sf.jsqlparser.expression.WhenClause;
-import net.sf.jsqlparser.expression.WithinGroupExpression;
-import net.sf.jsqlparser.expression.operators.arithmetic.Addition;
-import net.sf.jsqlparser.expression.operators.arithmetic.BitwiseAnd;
-import net.sf.jsqlparser.expression.operators.arithmetic.BitwiseOr;
-import net.sf.jsqlparser.expression.operators.arithmetic.BitwiseXor;
-import net.sf.jsqlparser.expression.operators.arithmetic.Concat;
-import net.sf.jsqlparser.expression.operators.arithmetic.Division;
-import net.sf.jsqlparser.expression.operators.arithmetic.Modulo;
-import net.sf.jsqlparser.expression.operators.arithmetic.Multiplication;
-import net.sf.jsqlparser.expression.operators.arithmetic.Subtraction;
-import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
-import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
-import net.sf.jsqlparser.expression.operators.relational.Between;
-import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
-import net.sf.jsqlparser.expression.operators.relational.ExistsExpression;
-import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
-import net.sf.jsqlparser.expression.operators.relational.GreaterThan;
-import net.sf.jsqlparser.expression.operators.relational.GreaterThanEquals;
-import net.sf.jsqlparser.expression.operators.relational.InExpression;
-import net.sf.jsqlparser.expression.operators.relational.IsNullExpression;
-import net.sf.jsqlparser.expression.operators.relational.ItemsListVisitor;
-import net.sf.jsqlparser.expression.operators.relational.LikeExpression;
-import net.sf.jsqlparser.expression.operators.relational.Matches;
-import net.sf.jsqlparser.expression.operators.relational.MinorThan;
-import net.sf.jsqlparser.expression.operators.relational.MinorThanEquals;
-import net.sf.jsqlparser.expression.operators.relational.MultiExpressionList;
-import net.sf.jsqlparser.expression.operators.relational.NotEqualsTo;
-import net.sf.jsqlparser.expression.operators.relational.RegExpMatchOperator;
-import net.sf.jsqlparser.expression.operators.relational.RegExpMySQLOperator;
-import net.sf.jsqlparser.schema.Column;
-import net.sf.jsqlparser.schema.Table;
-import net.sf.jsqlparser.statement.select.AllColumns;
-import net.sf.jsqlparser.statement.select.AllTableColumns;
-import net.sf.jsqlparser.statement.select.FromItemVisitor;
-import net.sf.jsqlparser.statement.select.LateralSubSelect;
-import net.sf.jsqlparser.statement.select.OrderByElement;
-import net.sf.jsqlparser.statement.select.OrderByVisitor;
-import net.sf.jsqlparser.statement.select.PlainSelect;
-import net.sf.jsqlparser.statement.select.SelectBody;
-import net.sf.jsqlparser.statement.select.SelectExpressionItem;
-import net.sf.jsqlparser.statement.select.SelectItem;
-import net.sf.jsqlparser.statement.select.SelectItemVisitor;
-import net.sf.jsqlparser.statement.select.SelectVisitor;
-import net.sf.jsqlparser.statement.select.SetOperationList;
-import net.sf.jsqlparser.statement.select.SubJoin;
-import net.sf.jsqlparser.statement.select.SubSelect;
-import net.sf.jsqlparser.statement.select.TableFunction;
-import net.sf.jsqlparser.statement.select.ValuesList;
-import net.sf.jsqlparser.statement.select.WithItem;
 
 public class QueryStripperVisitor implements ExpressionVisitor, FromItemVisitor, ItemsListVisitor, SelectVisitor, SelectItemVisitor, OrderByVisitor {
 	private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd"),
@@ -111,7 +38,23 @@ public class QueryStripperVisitor implements ExpressionVisitor, FromItemVisitor,
 		expression.accept(this);
 		return null;
 	}
-	
+
+	@Override
+	public void visit(BitwiseRightShift arg0) {
+		Expression e = visitExpression(arg0.getLeftExpression());
+		if (e != null) arg0.setLeftExpression(e);
+		e = visitExpression(arg0.getRightExpression());
+		if (e != null) arg0.setRightExpression(e);
+	}
+
+	@Override
+	public void visit(BitwiseLeftShift arg0) {
+		Expression e = visitExpression(arg0.getLeftExpression());
+		if (e != null) arg0.setLeftExpression(e);
+		e = visitExpression(arg0.getRightExpression());
+		if (e != null) arg0.setRightExpression(e);
+	}
+
 	@Override
 	public void visit(NullValue arg0) {
 
@@ -422,12 +365,6 @@ public class QueryStripperVisitor implements ExpressionVisitor, FromItemVisitor,
 	}
 
 	@Override
-	public void visit(WithinGroupExpression arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public void visit(ExtractExpression arg0) {
 		// TODO Auto-generated method stub
 
@@ -455,6 +392,11 @@ public class QueryStripperVisitor implements ExpressionVisitor, FromItemVisitor,
 	public void visit(JsonExpression arg0) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void visit(JsonOperator jsonExpr) {
+		// TODO Auto-generated method stub
 	}
 
 	@Override
@@ -488,6 +430,11 @@ public class QueryStripperVisitor implements ExpressionVisitor, FromItemVisitor,
 	}
 
 	@Override
+	public void visit(ValueListExpression valueList) {
+		valueList.getExpressionList().accept(this);
+	}
+
+	@Override
 	public void visit(RowConstructor arg0) {
 		// TODO Auto-generated method stub
 
@@ -509,6 +456,29 @@ public class QueryStripperVisitor implements ExpressionVisitor, FromItemVisitor,
 	public void visit(DateTimeLiteralExpression arg0) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void visit(NotExpression aThis) {
+		aThis.getExpression().accept(this);
+	}
+
+	@Override
+	public void visit(NextValExpression aThis) {
+
+	}
+
+	@Override
+	public void visit(CollateExpression aThis) {
+
+	}
+
+	@Override
+	public void visit(SimilarToExpression arg0) {
+		Expression e = visitExpression(arg0.getLeftExpression());
+		if (e != null) arg0.setLeftExpression(e);
+		e = visitExpression(arg0.getRightExpression());
+		if (e != null) arg0.setRightExpression(e);
 	}
 
 	@Override
@@ -540,12 +510,22 @@ public class QueryStripperVisitor implements ExpressionVisitor, FromItemVisitor,
 	}
 
 	@Override
+	public void visit(ParenthesisFromItem aThis) {
+		aThis.getFromItem().accept(this);
+	}
+
+	@Override
 	public void visit(ExpressionList arg0) {
 		List<Expression> expList = arg0.getExpressions();
 		for (int i = 0; i < expList.size(); i++) {
 			Expression e = visitExpression(expList.get(i));
 			if (e != null) expList.set(i, e);
 		}
+	}
+
+	@Override
+	public void visit(NamedExpressionList namedExpressionList) {
+		namedExpressionList.getExpressions().forEach(expression -> expression.accept(this));
 	}
 
 	@Override
@@ -569,7 +549,7 @@ public class QueryStripperVisitor implements ExpressionVisitor, FromItemVisitor,
 		if(arg0.getWhere()!=null) {		
 			arg0.getWhere().accept(this);
 		}
-		if(arg0.getGroupByColumnReferences() != null) arg0.getGroupByColumnReferences().stream().forEach(x -> x.accept(this));
+		if(arg0.getGroupBy() != null) arg0.getGroupBy().getGroupByExpressions().stream().forEach(x -> x.accept(this));
 		if(arg0.getOrderByElements() != null) {
 			for (OrderByElement obe : arg0.getOrderByElements()) {
 				obe.accept(this);
@@ -588,6 +568,11 @@ public class QueryStripperVisitor implements ExpressionVisitor, FromItemVisitor,
 	public void visit(WithItem arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void visit(ValuesStatement aThis) {
+
 	}
 
 	@Override
